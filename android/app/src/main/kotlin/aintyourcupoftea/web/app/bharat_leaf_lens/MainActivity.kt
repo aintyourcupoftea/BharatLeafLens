72f773671dc2b6382bcbc138abe5e948ef3e037f
Amit Gavali <shambho@Amits-MacBook-Air.local>
2024-02-22 11:13:40 +0530

    First commit

---
+++ android/app/src/main/kotlin/aintyourcupoftea/web/app/bharat_leaf_lens/MainActivity.kt
@@ -0,0 +1,6 @@
+package aintyourcupoftea.web.app.bharat_leaf_lens
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/homepage.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const String appTitle = 'Bharat Leaf Lens';
+    return MaterialApp(
+      title: appTitle,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text(
+              "Welcome to\nBharat Leaf Lens",
+              textAlign: TextAlign.center,
+              style: GoogleFonts.playfairDisplay(
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
